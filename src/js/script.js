18a8328962d120bcbae337499113cfea46c35d37
mohammedjammeh <mohammedjammeh@yahoo.com>
2023-06-05 11:09:42 +0100

    Update src files and add web3 wallet checks in script.js file.

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,40 @@
 import { ethers } from './ethers-5.1.esm.min.js';
 import { eventAddress, eventABI } from './constants/event.js';
 
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
-const contract = new ethers.Contract(eventAddress, eventABI, signer);
-
-async function connect() {
-	if (typeof window.ethereum == 'undefined') {
-		console.log('No metamask');
-	} else {
-		await window.ethereum.request({ method: 'eth_requestAccounts' });
-	}
+if (typeof window.ethereum == 'undefined') {
+	throw new Error('Please install Metamask!');
 }
 
-const main = async () => {
-	const all = await contract.getAll();
-	console.log(all);
-};
+// Variables (HTML Elements)
+const eventForm = document.getElementById('eventForm');
+const connectBtn = document.getElementById('connectBtn');
 
-connect();
+// Variables (Web3)
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const accounts = await provider.listAccounts();
+const accountExists = accounts.length > 0;
 
-main();
+// Functions
+async function connect() {
+	await window.ethereum.request({ method: 'eth_requestAccounts' });
+	connectedSetup();
+}
+
+async function connectedSetup() {
+	connectBtn.innerHTML = 'Disconnect From My Wallet';
+}
+
+if (!accountExists) {
+	eventForm.addEventListener('submit', connect);
+	connectBtn.addEventListener('click', connect);
+}
+
+// const main = async () => {
+// 	const signer = provider.getSigner();
+// 	const contract = new ethers.Contract(eventAddress, eventABI, signer);
+// 	const all = await contract.getAll();
+
+// 	console.log(all);
+// };
+
+// main();
