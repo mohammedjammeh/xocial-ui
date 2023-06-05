d2503dcf5b2564c41c0c709245dda3baee882990
mohammedjammeh <mohammedjammeh@yahoo.com>
2023-06-05 18:45:39 +0100

    Add more event listeners for when user wallet is not connected.

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,15 @@ if (typeof window.ethereum == 'undefined') {
 
 // Variables (HTML Elements)
 const eventForm = document.getElementById('eventForm');
+
+const homeBtn = document.getElementById('homeBtn');
+const profileBtn = document.getElementById('profileBtn');
 const connectBtn = document.getElementById('connectBtn');
+
+const broadcastBtns = document.querySelectorAll('.broadcastForm input');
+const joinBtns = document.querySelectorAll('.joinBtn button');
+
+const userSuggestionsBtns = document.querySelectorAll('.userSuggestions button');
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
@@ -28,10 +36,26 @@ async function connect() {
 // Application
 const accounts = await provider.listAccounts();
 if (accounts.length == 0) {
-	eventForm.addEventListener('submit', connect);
+	connectBtn.classList.remove('hide');
+
+	eventForm.addEventListener('submit', (event) => {
+		event.preventDefault();
+		connect();
+	});
+
+	homeBtn.addEventListener('click', connect);
+	profileBtn.addEventListener('click', connect);
 	connectBtn.addEventListener('click', connect);
 
-	connectBtn.classList.remove('hide');
+	broadcastBtns.forEach((btn) => {
+		btn.addEventListener('click', (event) => {
+			event.preventDefault();
+			connect();
+		});
+	});
+	joinBtns.forEach((btn) => btn.addEventListener('click', connect));
+
+	userSuggestionsBtns.forEach((btn) => btn.addEventListener('click', connect));
 }
 
 // const main = async () => {
