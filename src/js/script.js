aa2f845c69e6dbb5044d1008e52d895b05d64e4e
mohammedjammeh <mohammedjammeh@yahoo.com>
2023-06-12 16:25:30 +0100

    Update script js to work for profile forms.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 import { ethers } from './ethers-5.1.esm.min.js';
 import { linkupAddress, linkupABI } from './constants/linkup.js';
+import { userContractAddress, userContractABI } from './constants/user.js';
 
 if (typeof window.ethereum == 'undefined') {
 	throw new Error('Please install Metamask!');
@@ -7,9 +8,11 @@ if (typeof window.ethereum == 'undefined') {
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const linkupContract = new ethers.Contract(linkupAddress, linkupABI, provider.getSigner());
+const userContract = new ethers.Contract(userContractAddress, userContractABI, provider.getSigner());
 
 // Variables (HTML Elements)
 const linkupForm = document.getElementById('linkupForm');
+const profileForm = document.getElementById('profileForm');
 
 const homeBtn = document.getElementById('homeBtn');
 const profileBtn = document.getElementById('profileBtn');
@@ -34,13 +37,31 @@ async function connect() {
 	connectBtn.classList.add('hide');
 }
 
+function toggleDotDisplay(btn) {
+	let btnClasses = btn.classList;
+	btnClasses = Object.keys(btnClasses).map((key) => btnClasses[key]);
+
+	if (btnClasses.includes('attention')) {
+		btn.classList.remove('attention');
+
+		return;
+	}
+
+	btn.classList.add('attention');
+}
+
 /******************
 	Application
 ******************/
 const accounts = await provider.listAccounts();
-if (accounts.length == 0) {
-	connectBtn.classList.remove('hide');
 
+// unauthenticated
+if (accounts.length == 0) {
+	// connect button
+	connectBtn.classList.remove('hide');
+	setInterval(() => toggleDotDisplay(connectBtn), 800);
+
+	// connect for all buttons
 	linkupForm.addEventListener('submit', (event) => {
 		event.preventDefault();
 		connect();
@@ -86,25 +107,113 @@ linkupForm.addEventListener('submit', async (event) => {
 	);
 });
 
-// nav
+// const loadingDisplay = setInterval(() => {
+// 	toggleDotDisplay(connectBtn);
+// }, 800);
 
-function toggleDisplay(btn) {
-	let btnClasses = btn.classList;
-	btnClasses = Object.keys(btnClasses).map((key) => btnClasses[key]);
+// clearInterval(loadingDisplay);
 
-	if (btnClasses.includes('attention')) {
-		btn.classList.remove('attention');
+/******************
+	Profile
+******************/
+// user form
+let allUsers = await userContract.getAll();
+let clientAddress = accounts[0];
+// let clientAddress = '9826';
+let storedAccount = allUsers.find((user) => user.owner == clientAddress);
+
+let fullNameField = document.getElementById('fullName');
+let musicTasteFields = document.getElementsByName('musicTaste[]');
+
+if (storedAccount) {
+	// autofill Form
+	fullNameField.value = storedAccount['fullName'];
+	musicTasteFields.forEach((field) => {
+		if (storedAccount['musicTaste'].includes(field.value)) {
+			field.checked = true;
+		}
+	});
+} else {
+	// profile nav attention
+	profileBtn.children[1].classList.add('dot');
+	setInterval(() => toggleDotDisplay(profileBtn), 800);
+}
+
+profileForm.addEventListener('submit', async (event) => {
+	event.preventDefault();
+
+	// store profile form
+	let selectedMusicTaste = [];
+	musicTasteFields.forEach((field) => {
+		if (field.checked) {
+			selectedMusicTaste.push(field.value);
+		}
+	});
+
+	const response = await userContract.create(
+		'0x0A2169dfcC633289285290a61BB4d10AFA131817',
+		fullNameField.value,
+		selectedMusicTaste
+	);
+});
+
+// contact form
+let searchBtn = document.querySelectorAll('.search button')[0];
+let searchField = document.querySelectorAll('.search input')[0];
+let searchContainer = document.querySelectorAll('.contacts .search + .list')[0];
+
+searchBtn.addEventListener('click', async (event) => {
+	event.preventDefault();
+
+	let searchValue = searchField.value.toLowerCase();
+	searchContainer.innerHTML = '';
+
+	if (searchValue === '') {
+		return;
+	}
+
+	let searchUsers = allUsers.filter((user) => {
+		// return user.owner !== clientAddress && user.fullName.toLowerCase().includes(searchValue);
+		return user.fullName.toLowerCase().includes(searchValue);
+	});
+
+	if (searchUsers.length == 0) {
+		let messageContainer = document.createElement('div');
+		let messageElement = document.createElement('p');
+		messageElement.innerHTML = `There are no users with the name or address: ${searchValue}.`;
+
+		messageContainer.append(messageElement);
+		searchContainer.append(messageContainer);
 
 		return;
 	}
 
-	btn.classList.add('attention');
-}
+	searchUsers.forEach((user) => {
+		let searchElement = document.createElement('div');
 
-const loadingDisplay = setInterval(() => {
-	connectBtn.children[1].classList.add('dot');
+		let nameElement = document.createElement('p');
+		nameElement.classList.add('name');
+		nameElement.innerHTML = user.fullName;
 
-	toggleDisplay(connectBtn);
-}, 800);
+		let addressElement = document.createElement('p');
+		addressElement.classList.add('address');
+		addressElement.innerHTML = user.owner;
 
-// clearInterval(loadingDisplay);
+		let btnContainer = document.createElement('div');
+		btnContainer.classList.add('removeBtnContainer');
+
+		let btnElement = document.createElement('button');
+
+		let btnIconElement = document.createElement('i');
+		btnIconElement.classList.add('fa-solid');
+		btnIconElement.classList.add('fa-circle-plus');
+
+		btnElement.append(btnIconElement);
+		btnContainer.append(btnElement);
+
+		searchElement.append(nameElement);
+		searchElement.append(addressElement);
+		searchElement.append(btnContainer);
+		searchContainer.append(searchElement);
+	});
+});
